bede06883d15fe64720978863b5ee8e8ea3c03d2
quintans <paulo.quintans@gmail.com>
2019-10-02 22:53:37 +0100

    fix SimpleAbstractRowTransformer

--- transformers/SimpleAbstractRowTransformer.go
+++ transformers/SimpleAbstractRowTransformer.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 
 	"github.com/quintans/goSQL/dbx"
+	tk "github.com/quintans/toolkit"
 	coll "github.com/quintans/toolkit/collections"
 )
 
 type SimpleAbstractRowTransformer struct {
-	Transformer func(rows *sql.Rows) (tk.Hasher, error)
+	Transformer func(rows *sql.Rows) (interface{}, error)
 }
 
 var _ dbx.IRowTransformer = &SimpleAbstractRowTransformer{}
@@ -17,14 +18,14 @@ func (this *SimpleAbstractRowTransformer) BeforeAll() coll.Collection {
 	return coll.NewArrayList()
 }
 
-func (this *SimpleAbstractRowTransformer) Transform(rows *sql.Rows) (tk.Hasher, error) {
+func (this *SimpleAbstractRowTransformer) Transform(rows *sql.Rows) (interface{}, error) {
 	if this.Transformer != nil {
 		return this.Transformer(rows)
 	}
-	return nil, &tk.Fault{dbx.FAIL_STATEMENT, "Undefined Transformer function"}
+	return nil, &tk.Fail{dbx.FAULT_PARSE_STATEMENT, "Undefined Transformer function"}
 }
 
-func (this *SimpleAbstractRowTransformer) OnTransformation(result coll.Collection, instance tk.Hasher) {
+func (this *SimpleAbstractRowTransformer) OnTransformation(result coll.Collection, instance interface{}) {
 	if instance != nil {
 		result.Add(instance)
 	}
